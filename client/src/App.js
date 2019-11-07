04e78ad4c91062535294754d847d5a67a15c06cd
Luke Herczeg <lukeherczeg@ufl.edu>
2019-11-07 16:56:06 -0500

    Removed some of the template stuff.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./views/Home/Home"
 import NotFound from "./views/NotFound"
-import Header from "./components/Header/Header"
 
 
 const App = () => {
   return (
     <div>
-      <Header />
       <Switch>
         <Route exact path="/Home" component={Home} />
         <Route exact path="/">
