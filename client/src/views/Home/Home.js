038d23c2a856f1f4e3600c6626dbf6d098d6445c
Stefan Araujo <saraujo@ufl.edu>
2019-11-11 15:52:37 -0500

    Added Textboxes and design feautures made Login Buton also

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -14,11 +14,25 @@ function Home() {
                         <img className="paracosm-logo" src={logo} />
                         <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6"></i>
                     </a>
+                    <text>Username</text>
+                    <p>
+                    <input type="text" name="E-MAIL" id="E-MAIL"/>
+                    </p>
+                    <text>Password</text>
+                    <p><input type="text" name="Password" id="Password"/></p>
+            
+                <a class="Buttons">
+                    <Link to="/Login">
+             <button class="ripples" type="button">
+                    Login
+                     </button>
+            </Link>
             <Link to="/Register">
                 <button class="ripple" type="button">
                     Register
                 </button>
             </Link>
+            </a>
             </div>
             </div>
     );
--- client/src/views/Register/Register.js
+++ client/src/views/Register/Register.js
@@ -1,20 +1,32 @@
 import React,{Component} from 'react';
-import{Redirect,Link,NavLink,BrowserRouter as Router} from 'react-router'
+import{Redirect,Link,NavLink,BrowserRouter as Router} from 'react-router-dom'
 import logo from '../../assets/paracosm.png';
 import './Register.css';
 //import { Link } from 'react-router-dom';
 function Register() {
     return (
         <div className="App">
-            <header className="App-header">
-                <div className="App-paracosm">
+                <div className="App-header">
                 {/* Logo */}
                     <a className="App-logo" target='_blank' rel="noopener noreferrer" href="https://paracosm.io">
                         <img className="paracosm-logo" src={logo} />
                         <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6"></i>
                     </a>
+                    <text>Enter E-Mail</text>
+                    <p>
+                    <input type="text" name="E-MAIL" id="E-MAIL"/>
+                    </p>
+                    <text>Enter Password</text>
+                    <p><input type="text" name="Password" id="Password" /></p>
+                    <text>Confirm Password</text>
+                    <p><input type="text" name="Confirm-Password" id="Confirm-Password" /></p>
+                    <Link to="/Home">
+                <button class="ripple" type="button">
+                    Submit
+                </button>
+                </Link>
                     </div>
-            </header>
+
         </div> 
     );
 }
