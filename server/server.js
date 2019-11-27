b35994f6f7773b0deb824ed72271b5b17fadbb41
Chandler Trousdale <ctrousdale@ufl.edu>
2019-11-27 14:33:08 -0500

    Changed port to client config

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express'),
       upload = require('./upload'),
       cors = require('cors');
 var bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 const server = express();
 
