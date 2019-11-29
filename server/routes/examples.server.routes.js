f09f8d1a413834cca2c0b02018ff8552e51123c7
Chandler Trousdale <ctrousdale@ufl.edu>
2019-11-29 00:58:26 -0500

    Testing printing through the server

--- server/routes/examples.server.routes.js
+++ server/routes/examples.server.routes.js
@@ -1,8 +1,15 @@
-const examples = require('../controllers/examples.server.controller.js'),
-    express = require('express'), 
-    router = express.Router()
+//const examples = require('../controllers/examples.server.controller.js'),
+//    express = require('express'),
+//    router = express.Router()
+//
+//router.route('/')
+//  .get(examples.hello);
+//
+//module.exports = router;
 
-router.route('/')
-  .get(examples.hello);
-  
-module.exports = router;
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res)  {
+  res.send('Hello World!')
+})
