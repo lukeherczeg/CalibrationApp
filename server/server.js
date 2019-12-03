c46c2205cc8fb56beb221ed5f17a0dd8cb68b922
Luke Herczeg <lukeherczeg@ufl.edu>
2019-12-03 04:19:22 -0500

    Merged it all

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express'),
+      upload = require('./upload'),
+      cors = require('cors');
+var bodyParser = require('body-parser');
+
+const server = express();
+
+
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+}
+
+server.use(cors(corsOptions));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+server.use(express.json());
+server.use(express.urlencoded());
+
+server.post('/FileUpload', FileUpload)
+
+server.post('/upload', upload);
+
+server.post('/uuid', function (req, res) {
+    const uuid = req.body.uuid;
+    exports.uuid = uuid;
+});
+
+server.listen(5000, () => {
+    console.log('Server started!');
+});
