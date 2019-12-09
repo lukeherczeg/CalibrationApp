8f09bc0ee470bf0150771cbf141e58f74716e835
lukeherczeg <lukeherczeg@ufl.edu>
2019-12-09 13:42:33 -0500

    revert ftt 2,3,4

--- client/src/views/FileUpload/Upload.js
+++ client/src/views/FileUpload/Upload.js
@@ -160,10 +160,10 @@ class Upload extends Component {
   render() {
 
     {/* First populate an array of files to view*/}
-    {/*let Display = await this.state.viewFiles;*/}
+    var Display = await this.state.viewFiles;
     {/* Grab the first element of the array of files, and UUID = first element of the split key*/}
     var uuid = (this.state.gotFiles ?
-                'Files in UUID: ' + this.fileDisplay.map((item, index) => (item.Key.split("/")[0]))[0] :
+                'Files in UUID: ' + Display.map((item, index) => (item.Key.split("/")[0]))[0] :
                 ' ');
 
 
@@ -208,7 +208,7 @@ class Upload extends Component {
          {uuid}
         </span>
         <div className="viewFiles">
-          {this.fileDisplay.map((item, index) => (
+          {Display.map((item, index) => (
               <p> {item.Key.split("/")[1]}  </p>
           ))}
         </div>
